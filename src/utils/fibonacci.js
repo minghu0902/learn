bf2c6c566bbcf6ce834eabb26ab483cc80d561d2
unknown <3173329910@qq.com>
2019-11-17 22:37:19 +0800

    update fib

--- src/utils/fibonacci.js
+++ src/utils/fibonacci.js
@@ -1,6 +1,6 @@
 
 
-function fibonacci(n) {
+function fibonacci_1(n) {
   let first = 1
   let second = 2
   let result = []
@@ -14,7 +14,16 @@ function fibonacci(n) {
 }
 
 
-function* fibonacci() {
+function fibonacci_2(n, first=1, second=2) {
+  if (n <= 1) {
+    return second
+  }
+  console.log(first)
+  return fibonacci_2(n-1, second, first + second)
+}
+
+
+function* fibonacci_3() {
   let first = 1
   let second = 2
   while(true) {
@@ -23,7 +32,7 @@ function* fibonacci() {
   }
 }
 
-// let f = fibonacci()
+// let f = fibonacci_3()
 // let n = 20
 // while(n--) {
 //   console.log(f.next())
