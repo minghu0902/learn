2df818bc21c7a299e7a99f6ca1a9e030332c46dc
unknown <3173329910@qq.com>
2020-04-02 23:57:15 +0800

    update promise.js

--- src/promise/promise.js
+++ src/promise/promise.js
@@ -147,6 +147,13 @@
         },
         catch: function(onRejected) {
             return this.then(null, onRejected);
+        },
+        finaly: function (callback) {
+            const P = this.constructor
+            return this.then(
+                value => P.resolve(callback()).then(() => value),
+                reason => P.resolve(callback()).then(() => {}, () => { throw reason })
+            )
         }
     }
 
